36caf09a7bec1913b206eee36d6e2e8cbc82bc30
Ajay K <ariashievan@gmail.com>
2025-10-14 18:51:28 +0530

    Added webview WIP

--- app/src/main/java/com/example/myapplication/Webview.java
+++ app/src/main/java/com/example/myapplication/Webview.java
@@ -1,9 +1,6 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,22 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class HomePage extends AppCompatActivity {
-    Button submitbuttonhome;
+public class Webview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.home_page);
-        submitbuttonhome=findViewById(R.id.submitbuttonhome);
-        submitbuttonhome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), Webview.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_webview);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
