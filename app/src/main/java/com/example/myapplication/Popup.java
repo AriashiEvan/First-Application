413e6f35fed6a49304a4ca3ae3843c1751c21a1b
Ajay K <ariashievan@gmail.com>
2025-10-15 15:58:45 +0530

    Made a popup dialogue with dialogue class

--- app/src/main/java/com/example/myapplication/Popup.java
+++ app/src/main/java/com/example/myapplication/Popup.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.app.Dialog;
 import android.os.Bundle;
-import android.webkit.WebView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +12,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Webview extends AppCompatActivity {
-    WebView HomeWeb;
-
+public class Popup extends AppCompatActivity {
+    Button ButtonPopUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_webview);
-        HomeWeb=findViewById(R.id.viewweb);
-        HomeWeb.getSettings().setJavaScriptEnabled(true);
-        HomeWeb.loadUrl("https://google.com/");
+        setContentView(R.layout.activity_popup);
+        ButtonPopUp=findViewById(R.id.popupbutton);
+        ButtonPopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog=new Dialog(Popup.this);
+                dialog.setContentView(R.layout.custom_pop);
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
